d4902a83bc239a4df0202d5fb394a54262eebddf
yazdanaq <qadriyazdan91@gmail.com>
2025-07-15 15:41:14 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -412,3 +412,9 @@ tabButtons.forEach(button => {
 });
 
 
+// Dynamically set the "Last Updated" date
+document.getElementById("last-updated").textContent = new Date(document.lastModified).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
